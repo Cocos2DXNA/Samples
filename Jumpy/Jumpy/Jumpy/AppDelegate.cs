43c1b0fbef0ce4b16d33b0c509e3231b6bdc7adf
TotallyEvil Developers <dev@totallyevil.com>
2013-08-27 06:47:58 -0700

    initial commit of the jumpy test

---
+++ Jumpy/Jumpy/Jumpy/AppDelegate.cs
@@ -0,0 +1,128 @@
+// AppDelegate.cs
+//
+// Author(s)
+//	Stephane Delcroix <stephane@delcroix.org>
+//
+// Copyright (C) 2012 s. Delcroix
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+// and associated documentation files (the "Software"), to deal in the Software without restriction, 
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+// subject to the following conditions:
+//		
+// 		The above copyright notice and this permission notice shall be included in all copies or 
+//		substantial portions of the Software.
+//		
+//		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+//		BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+//		NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+//		DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+//		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Cocos2D;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+namespace Jumpy
+{
+	// The UIApplicationDelegate for the application. This class is responsible for launching the 
+	// User Interface of the application, as well as listening (and optionally responding) to 
+	// application events from iOS.
+	public partial class AppDelegate : CCApplication
+	{
+        public AppDelegate(Game game, GraphicsDeviceManager graphics)
+            : base(game, graphics)
+        {
+            s_pSharedApplication = this;
+            CCDrawManager.InitializeDisplay(game, graphics, DisplayOrientation.LandscapeRight | DisplayOrientation.LandscapeLeft);
+
+
+            graphics.PreferMultiSampling = false;
+        }
+
+        /// <summary>
+        ///  Implement CCDirector and CCScene init code here.
+        /// </summary>
+        /// <returns>
+        ///  true  Initialize success, app continue.
+        ///  false Initialize failed, app terminate.
+        /// </returns>
+        public override bool ApplicationDidFinishLaunching()
+        {
+            //initialize director
+            CCDirector pDirector = CCDirector.SharedDirector;
+            pDirector.SetOpenGlView();
+#if WINDOWS
+            CCDrawManager.SetDesignResolutionSize(320, 480, CCResolutionPolicy.ExactFit);
+#else
+            CCDrawManager.SetDesignResolutionSize(800, 480, ResolutionPolicy.ShowAll);
+            //CCDrawManager.SetDesignResolutionSize(480, 320, ResolutionPolicy.ShowAll);
+#endif
+            // turn on display FPS
+            pDirector.DisplayStats = true;
+
+            // set FPS. the default value is 1.0/60 if you don't call this
+            pDirector.AnimationInterval = 1.0 / 60;
+
+            // create a scene. it's an autorelease object
+            CCScene pScene = new CCScene();
+            CCLayer pLayer = new GameLayer();
+
+            pScene.AddChild(pLayer);
+            pDirector.RunWithScene(pScene);
+
+            return true;
+        }
+
+        protected override PresentationParameters WindowSetup
+        {
+            get
+            {
+                PresentationParameters pp = new PresentationParameters();
+                pp.BackBufferFormat = SurfaceFormat.Color;
+                pp.BackBufferHeight = 800;
+                pp.BackBufferWidth = 600;
+                pp.DepthStencilFormat = DepthFormat.Depth24Stencil8;
+                pp.RenderTargetUsage = RenderTargetUsage.PreserveContents;
+                return (pp);
+            }
+        }
+        /// <summary>
+        /// We need to override this device settings callback so that the correct back buffer it set.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected override void GraphicsPreparingDeviceSettings(object sender, PreparingDeviceSettingsEventArgs e)
+        {
+            base.GraphicsPreparingDeviceSettings(sender, e);
+//            e.GraphicsDeviceInformation.PresentationParameters.BackBufferHeight = 960;
+//            e.GraphicsDeviceInformation.PresentationParameters.BackBufferWidth = 800;
+        }
+
+        /// <summary>
+        /// The function be called when the application enter background
+        /// </summary>
+        public override void ApplicationDidEnterBackground()
+        {
+            CCDirector.SharedDirector.Pause();
+
+            // if you use SimpleAudioEngine, it must be pause
+            // SimpleAudioEngine::sharedEngine()->pauseBackgroundMusic();
+        }
+
+        /// <summary>
+        /// The function be called when the application enter foreground  
+        /// </summary>
+        public override void ApplicationWillEnterForeground()
+        {
+            CCDirector.SharedDirector.Resume();
+
+            // if you use SimpleAudioEngine, it must resume here
+            // SimpleAudioEngine::sharedEngine()->resumeBackgroundMusic();
+        }
+    }
+}
+
